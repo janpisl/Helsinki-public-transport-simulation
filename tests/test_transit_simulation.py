cf33f4a3b89a1722fd24d64e6910aab9af71e902
Jarmo Kivekäs <jarmo.kivekas@gmail.com>
2020-04-29 19:01:14 +0300

    amend tests

--- tests/test_transit_simulation.py
+++ tests/test_transit_simulation.py
@@ -5,7 +5,7 @@
 import pytest
 
 import geopandas as gpd
-from transit_simulation import simulation as ts
+import transit_simulation as ts
 from shapely.geometry import LineString, Point
 
 
@@ -22,21 +22,21 @@ def test_next_location_along_route():
     route = LineString([(0, 300), (150, 300), (200, 350)])
 
     # current location in already on a route node
-    assert ts.next_location_along_route(Point(0,300), route, 50) == Point(50,300)
+    assert ts.simulation.next_location_along_route(Point(0,300), route, 50) == Point(50,300)
     # current location is on the route, but not on a route node
-    assert ts.next_location_along_route(Point(10,300), route, 50) == Point(60,300)
-    assert ts.next_location_along_route(Point(2,300), route, 50) == Point(52,300)
+    assert ts.simulation.next_location_along_route(Point(10,300), route, 50) == Point(60,300)
+    assert ts.simulation.next_location_along_route(Point(2,300), route, 50) == Point(52,300)
     # current location is not at all on the route, it is snapped to the route
     # before determining the next location
-    assert ts.next_location_along_route(Point(10,303), route, 50) == Point(60,300)
+    assert ts.simulation.next_location_along_route(Point(10,303), route, 50) == Point(60,300)
     # due to float inaccuracies, can't use `==` comparison. doing point-in-polygon check insead
-    assert Point(50.5,300).buffer(0.0000001).contains(ts.next_location_along_route(Point(0,300), route, 50.5))
+    assert Point(50.5,300).buffer(0.0000001).contains(ts.simulation.next_location_along_route(Point(0,300), route, 50.5))
 
 def test_second_to_hour():
-    assert ts.second_to_hour(3600) == 1
+    assert ts.simulation.second_to_hour(3600) == 1
 
 def test_kmh_to_ms():
-    assert ts.kmph_to_mps(3.6) == 1
+    assert ts.simulation.kmph_to_mps(3.6) == 1
 
 def test_calculate_travel_time():
 
@@ -49,8 +49,8 @@ def test_calculate_travel_time():
     # the test route is about 4.5 km. The unit should be meters
     assert 4000 < route['geometry'].length.sum() < 5000
 
-    travel_time = ts.calculate_travel_time(route)
-    time_h = ts.second_to_hour(travel_time)
+    travel_time = ts.simulation.calculate_travel_time(route)
+    time_h = ts.simulation.second_to_hour(travel_time)
 
     # travel 4.5 km at 40 km/h takes about 0.11 hours
     assert 0.10 < time_h < 0.12
