76380733d3c1d3994f505811424878cd524726b0
Jan Pisl <jan.pisl@simosol.fi>
2020-05-04 15:34:18 +0200

    First draft of the agent objects

---
+++ transit_simulation/vehicle.py
@@ -0,0 +1,30 @@
+import uuid
+
+class Vehicle:
+    """
+    
+    """
+    def __init__(self, origin, destination):
+        self.id = uuid.uuid4()
+        self.current_location = origin
+        self.route = self.find_route(origin, destination)
+
+    def tick(self):
+        self.current_location = move()
+
+    def move(self):
+        raise NotImplementedError()
+    
+    def find_route(self, origin, destination):
+
+
+
+class Bus(Vehicle):
+    def __init__(self, origin, destination):
+        super().__init__(origin, destination)
+    
+
+class Car(Vehicle):
+    def __init__(self, origin, destination):
+        super().__init__(origin, destination)
+    
