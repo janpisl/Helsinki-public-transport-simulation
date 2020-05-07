25c5b97a2807dde0ddcc79cd2418b769bfb1f4e3
Jarmo Kivekäs <jarmo.kivekas@gmail.com>
2020-05-07 08:38:55 +0300

    simulation: add agent init and destruc logic

--- transit_simulation/simulation.py
+++ transit_simulation/simulation.py
@@ -5,6 +5,7 @@ import geopandas as gpd
 import numpy as np
 from shapely.geometry import LineString, Point
 
+EUREF_FIN_TM35_FIN_EPSG  = 'EPSG:102139'
 
 def kmph_to_mps(speed_kms: float):
     """Helper function, given km/h, retun m/s"""
@@ -32,25 +33,39 @@ def next_location_along_route(current_location: Point, route: LineString, distan
 def start_simulation(data_dir: str):
     """simulation entry point, handel all simulation functions"""
 
-    # data input
-    route = gpd.read_file(route_file)
-
-    # data analysis
-    time = calculate_travel_time(route)
-
-    return time
-
-    environment = load_environment_attr_layers()
+    environment = {
+        'speed_limit': gpd.read_file(f'{data_dir}/env_speed_limit.geojosn').to_crs(EUREF_FIN_TM35_FIN_EPSG)
+    }
     # shall be a file with linestirng geometry, each line has uinque id attribute
     # may be used for both generic agetns, and public transport
     # if routes get big, use geopackage instead. But this is convenient for debugging
-    routes = gpd.read_file(f'{data_dir}/routes.geojson')
+    routes = gpd.read_file(f'{data_dir}/routes.geojson').to_crs(EUREF_FIN_TM35_FIN_EPSG)
+
     # table of timestamps (iso8601 sting, or seconds since an epoch)
     schedule = gpd.pd.read_csv(f'{data_dir}/schedule.csv')
 
-    agents = []
 
-    
+    agents = []
+    sim_time_max = 500   # how many seconds in a simulation
+    tick_len = 1         # how many seconds in a tick
+    tick_current = 0
+    sim_time = 0
+    while sim_time < sim_time_max:
+
+        new_agents = schedule[sim_time <= schedule.start_time < sim_time + tick_len]
+
+        for new_agent in new_agents:
+            agents.append(Agent(
+                route = routes[new_agent.route_id],
+            ))
+
+        for idx, agent in agents:
+            agent.tick(tick_len)
+            del agents[idx] if agent.done else pass
+
+
+        sim_time += tick_len
+
 
 def calculate_travel_time(route) -> float:
     """given a pandas dataframe with line geometry, calculate the total travel time
