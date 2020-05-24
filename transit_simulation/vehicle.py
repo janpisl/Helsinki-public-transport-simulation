5ba1e44b589908bace22335eb9b376c9270a9ea6
jarvena <antti.p.jarvenpaa@aalto.fi>
2020-05-24 19:28:22 +0300

    Updated to master, added for vehicle a method to obtaine congested speed

--- transit_simulation/vehicle.py
+++ transit_simulation/vehicle.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from shapely.geometry import Point
 
 import transit_simulation.read_speed_limit_at_location as read_speed
+import transit_simulation.congestion_model as congestion
 
 class Vehicle:
     """
@@ -27,8 +28,16 @@ class Vehicle:
 
     def check_agent_speed_limit(self):
         speed_data_path = Path("./tests/test_data/speed_data_digi")
-        max_speed = read_speed.check_speed_limit_at_location(speed_data_path / "test_speed_data.geojson", self.current_location)
+        max_speed, _ = read_speed.check_speed_limit_at_location(speed_data_path / "test_speed_data.geojson", self.current_location)
         return max_speed
+        
+    def check_agent_congested_speed(self, time_of_day):
+        '''time_of_day: Hours elapsed since midnight'''
+        speed_data_path = Path("./tests/test_data/speed_data_digi")
+        speed_limit, n_road_elements = read_speed.check_speed_limit_at_location(speed_data_path / "test_speed_data.geojson", self.current_location)
+        congested_speed = congestion.congested_speed(speed_limit, time_of_day, n_road_elements)
+        
+        return congested_speed
     
 
 
