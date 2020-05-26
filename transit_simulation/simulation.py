80dd8d5e24f43b8bdf3de58535a0c80a70c6cc5c
Jarmo Kivekäs <jarmo.kivekas@gmail.com>
2020-05-26 20:59:56 +0300

    Integrate commandline interface, simulation, and agent modules
    - improve result output
    - enable running simulation using 'pytho3 -m transit_simulation.cli'
    - compatible with schedule.csv and routes files from gtfs
    - add support to hh:mm:ss timestamp format from schdule.csv
    - amend test cases

--- transit_simulation/simulation.py
+++ transit_simulation/simulation.py
@@ -5,7 +5,8 @@ import geopandas as gpd
 import numpy as np
 from typing import List
 from shapely.geometry import LineString, Point
-from transit_simulation.agent import MockAgent, create_agent
+from transit_simulation.vehicle import create_agent
+from assert_types import assert_types
 
 EUREF_FIN_TM35_FIN_EPSG  = 'EPSG:102139'
 ETRS89_TM35_FIN_EPSG  = 'EPSG:3067'
@@ -23,7 +24,15 @@ def second_to_hour(time_s: float):
     time_h = time_s / 60 / 60
     return time_h
 
-
+def time_of_day_to_seconds(timestamp:str):
+    """Given a string of format 'hh:mm:ss', return the number of seconds since start of day (00:00:00)
+    This format is used in the schedule.csv input data file"""
+    assert isinstance(timestamp, str)
+    timestamp = timestamp.split(':')
+    assert len(timestamp) == 3
+    timestamp = int(timestamp[0]) *60*60 + int(timestamp[1]) * 60 + int(timestamp[2])
+    assert 0 <= timestamp <= 86400 # number of seconds in a day
+    return timestamp
 
 def next_location_along_route(current_location: Point, route: LineString, distance: float) -> Point:
     """Calculate where the `next_location` is when travelling `distance` along `route` from `location`
@@ -43,41 +52,41 @@ def agents_to_gdf(agents:List) -> gpd.GeoDataFrame:
     snapshot = gpd.GeoDataFrame(geometry = points, crs=GEOM_PROCESSING_CRS)
     return snapshot
 
+
 def start_simulation(data_dir: str, start_time:float, end_time:float, tick_len:float):
-    """simulation entry point, handel all simulation functions"""
-    logger.debug('reading simulation environment')
-    environment = {
-        'speed_limit': gpd.read_file(f'{data_dir}/env_speed_limit.geojson').to_crs(GEOM_PROCESSING_CRS)
-    }
+    """simulation entry point, handel all simulation functions. This function is
+    called from the comandline utility"""
+
 
     # shall be a file with linestirng geometry, each line has uinque id attribute
     # may be used for both generic agetns, and public transport
     # if routes get big, use geopackage instead. But this is convenient for debugging
-    logger.debug('reading agent route geometries')
+    logger.info('reading agent route geometries')
     routes = gpd.read_file(f'{data_dir}/routes.geojson').to_crs(GEOM_PROCESSING_CRS)
     logger.debug(routes.columns)
 
-    # table of timestamps (iso8601 sting, or seconds since an epoch)
+    # table of timestamps (iso8601 time without date) for departing agents
     logger.debug('reading agent schedule')
-    schedule_df = gpd.pd.read_csv(f'{data_dir}/schedule.csv')
+    schedule_df = gpd.pd.read_csv(f'{data_dir}/schedule.csv', comment='#')
+    # convert time to seconds since start of day, to suit simulation
+    schedule_df['d_time'] = schedule_df['d_time'].apply(time_of_day_to_seconds)
 
+    sim_time = start_time       # timulation time, seconds since an epoch
+    agents = []                 # the list of agents currently in the simulation
+    snapshots = []              # for aggregating agent histories
 
-    agents = []          # the list of agents currently in the simulation
-    sim_time = start_time         # timulation time, seconds since an epoch
-
-    logger.debug('starting simulation')
+    logger.debug('entering main simulation loop')
     while sim_time < end_time:
         # check the schelude of new agents and add them to the simulation
-        departures = schedule_df[(sim_time <= schedule_df.start_time) & (schedule_df.start_time < sim_time + tick_len)]
+        departures = schedule_df[(sim_time <= schedule_df.d_time) & (schedule_df.d_time < sim_time + tick_len)]
         logger.debug(f'simulation time: {sim_time}')
         logger.debug(f'new departures: {len(departures)}')
 
-        for idx, departure in departures.iterrows():
-            logger.debug(departure.route_id)
-            logger.debug(routes.route_id)
+        for idx, schd_entry in departures.iterrows():
+            logger.debug(f"departing agent: {schd_entry.shape_id}, route: {routes.shape_id}")
             new_agent = create_agent(
-                route = routes[routes.route_id == departure.route_id].geometry.iloc[0 ],
-                type = departure.type
+                route = routes[routes.shape_id == schd_entry.shape_id].geometry.iloc[0 ],
+                agent_type = schd_entry.route_type
             )
             agents.append(new_agent)
 
@@ -91,11 +100,18 @@ def start_simulation(data_dir: str, start_time:float, end_time:float, tick_len:f
         if len(agents) != 0:
             snapshot = agents_to_gdf(agents)
             snapshot['timestamp'] = sim_time
-            snapshot = snapshot.to_crs(WGS84)
-            snapshot.to_file(f'{data_dir}/snapshot_{sim_time}.geojson', driver="GeoJSON")
+            # useful for debugging, gut creates HUGE amouns of files
+            # snapshot.to_file(f'{data_dir}/snapshot_{sim_time}.gpkg', driver='GPKG')
+            snapshots.append(snapshot)
 
         sim_time += tick_len
 
+    logger.info("Post-processing simulation data")
+    logger.info("number of snapshots: " + str(len(snapshots)))
+    result = gpd.pd.concat(snapshots)
+    result.to_file(f'{data_dir}/snapshots.gpkg', driver="GPKG")
+
+    return True
 
 def calculate_travel_time(route) -> float:
     """given a pandas dataframe with line geometry, calculate the total travel time
