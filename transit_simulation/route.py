721fbe69ae7df6580f98a9566a4204bb48a000d0
Jarmo Kivekäs <jarmo.kivekas@gmail.com>
2020-05-27 14:13:44 +0300

    add cli to gtfs route pre-processing

--- transit_simulation/route.py
+++ transit_simulation/route.py
@@ -5,6 +5,11 @@ import requests
 from io import BytesIO
 from zipfile import ZipFile
 from pathlib import Path
+from loguru import logger
+
+
+import warnings; warnings.filterwarnings('ignore', 'GeoSeries.notna', UserWarning)
+
 
 def fetch_route_rt(bus_route_id, direction, d_date, d_time):
     '''
@@ -13,7 +18,7 @@ def fetch_route_rt(bus_route_id, direction, d_date, d_time):
         direction - Direction of the trip, possible values: 0, 1 or -1. -1 indicates that the direction is irrelevant, i.e. in case the route has trips only in one direction.
         date - Departure date of the trip, format: YYYY-MM-DD
         time - Departure time of the trip, format: seconds since midnight of the departure date
-    
+
     Returns:
         route - A list of points describing the shape of the route.
     '''
@@ -27,7 +32,7 @@ def fetch_route_rt(bus_route_id, direction, d_date, d_time):
     '''.format(bus_route_id, direction, d_date, d_time)
     request = requests.post(api_url, json={'query': query})
     route = request.json()['data']['route']['geometry']
-    
+
     return route
 
 def fetch_HSL_gtfs_shapes():
@@ -36,11 +41,11 @@ def fetch_HSL_gtfs_shapes():
         file of a GTFS package.
     '''
     url = 'http://dev.hsl.fi/gtfs/hsl.zip'
-    
+
     r = requests.get(url)
     gtfs_zip = ZipFile(BytesIO(r.content))
     shape_df = pd.read_csv(gtfs_zip.open('shapes.txt'))
-    
+
     return shape_df
 
 def fetch_HSL_gtfs_data():
@@ -53,24 +58,24 @@ def fetch_HSL_gtfs_data():
         file of a GTFS package.
     route_df: A Pandas dataframe containing the information from the routes.txt
         file of a GTFS package.
-    
+
     '''
     url = 'http://dev.hsl.fi/gtfs/hsl.zip'
-    
+
     r = requests.get(url)
     gtfs_zip = ZipFile(BytesIO(r.content))
     shape_df = pd.read_csv(gtfs_zip.open('shapes.txt'))
     trips_df = pd.read_csv(gtfs_zip.open('trips.txt'))
     calendar_df = pd.read_csv(gtfs_zip.open('calendar.txt'))
     route_df = pd.read_csv(gtfs_zip.open('routes.txt'))
-    
+
     return shape_df, trips_df, calendar_df, route_df
 
 def process_gtfs_shapes(shapes_data):
     '''Args:
     shapes_data: A pandas DataFrame containing data of shapes.txt in a gtfs package.
         Practically route ids and points along the route.
-    
+
     Returns:
 
     routes: A GeoPandas GeoDataFrame containing unique shape ids and the corresponding
@@ -90,7 +95,7 @@ def process_gtfs_schedule(trips_df, calendar_df, route_df, simulation_date=pd.to
     route_df: A Pandas dataframe containing the information from the routes.txt
         file of a GTFS package.
     simulation_date: A datetime object defining from which date to fetch the schedule data.
-        
+
     Returns:
     schedule: Pandas dataframe containing shape ids, vehicle departure times and vehicle types
         for agent initalization.'''
@@ -98,19 +103,19 @@ def process_gtfs_schedule(trips_df, calendar_df, route_df, simulation_date=pd.to
     df.start_date = pd.to_datetime(df.start_date, format='%Y%m%d'); df.end_date = pd.to_datetime(df.end_date, format='%Y%m%d')
     df.loc[df.monday==1, 'weekday'] = 0; df.loc[df.tuesday==1, 'weekday'] = 1; df.loc[df.wednesday==1, 'weekday'] = 2; df.loc[df.thursday==1, 'weekday'] = 3; df.loc[df.friday==1, 'weekday'] = 4; df.loc[df.saturday==1, 'weekday'] = 5; df.loc[df.sunday==1, 'weekday'] = 6
     df = df[(df.weekday==simulation_date.weekday()) & (df.start_date<=simulation_date) & (df.end_date>=simulation_date)]
-    
+
     df['d_time'] = df.trip_id.str[-4:]
     df['d_time'] = pd.to_datetime(df['d_time'], errors='coerce', format='%H%M').dt.time
-    
+
     df = pd.merge(df, route_df[['route_id', 'route_type']], on='route_id', how='left')
     schedule = df.reset_index().loc[:,['shape_id', 'd_time', 'route_type']]
-    
+
     return schedule.dropna()
-	
+
 def create_initialization_data(simulation_date=pd.to_datetime('today')):
 	'''Args:
     simulation_date: Pandas datetime object defining the date for which the simulation will be run.
-    
+
     Returns:
 	route_df: A GeoPandas GeoDataFrame containing unique shape ids and the corresponding
         route LineString as geometry.
@@ -119,18 +124,19 @@ def create_initialization_data(simulation_date=pd.to_datetime('today')):
 	shape_df, trips_df, calendar_df, route_df = fetch_HSL_gtfs_data()
 	route_df = process_gtfs_shapes(shape_df)
 	schedule_df = process_gtfs_schedule(trips_df, calendar_df, route_df, simulation_date=simulation_date)
-	
+
 	return route_df, schedule_df
 
 
-def initialize_random_routes(sample_nr=20, simulation_date=pd.to_datetime('today')): # if the sample_nr is less or equal to 0, all samples are used
-    print('Fetching data')
+def initialize_random_routes(sample_nr=20, simulation_date=pd.to_datetime('today'), data_path="./tests/test_data"): # if the sample_nr is less or equal to 0, all samples are used
+    logger.info('Fetching data')
     shapes_df, trips_df, calendar_df, route_df = fetch_HSL_gtfs_data()
-    print('Processing data')
+    logger.info('Processing data')
     routes = process_gtfs_shapes(shapes_df)
 
     # read simulation extent from geojson
-    data_path = Path("./tests/test_data")
+    logger.debug(f'using data path: {data_path}')
+    data_path = Path(data_path)
     simulation_extent = gpd.read_file(data_path / 'simulation_extent.geojson')
 
     # create data frame with routes transformed to metric crs
@@ -180,24 +186,24 @@ def initialize_random_routes(sample_nr=20, simulation_date=pd.to_datetime('today
     # write generated routes to file
     # output_path = str(data_path / 'random_routes2.geojson')
     # sample_routes.to_file(output_path, driver="GeoJSON")
-    
+
     schedule = process_gtfs_schedule(trips_df, calendar_df, route_df, simulation_date)
     shape_ids = sample_routes.index
     sample_schedule = schedule[schedule.shape_id.isin(shape_ids)]
 
     return sample_routes, sample_schedule
 
-def initdata_to_files(routes, schedule):
-    data_path = Path("./tests/test_data")
+def initdata_to_files(routes, schedule, data_path="./tests/test_data"):
+    data_path = Path(data_path)
     schedule = schedule.set_index('shape_id')
 
     routes.to_file(str(data_path / 'routes.geojson'), driver="GeoJSON")
     schedule.to_csv(str(data_path / 'schedule.csv'))
     return True
-    
-def preprocess_initdata_to_files(sample_nr=20, simulation_date=pd.to_datetime('today')):
-    data_path = Path("./tests/test_data")
-    routes, schedule = initialize_random_routes(sample_nr, simulation_date)
+
+def preprocess_initdata_to_files(sample_nr=20, simulation_date=pd.to_datetime('today'), data_path='./tests/test_data'):
+    data_path = Path(data_path)
+    routes, schedule = initialize_random_routes(sample_nr, simulation_date, data_path)
     schedule = schedule.set_index('shape_id')
 
     routes.to_file(str(data_path / 'routes.geojson'), driver="GeoJSON")
@@ -205,26 +211,46 @@ def preprocess_initdata_to_files(sample_nr=20, simulation_date=pd.to_datetime('t
     return True
 
 if __name__ == "__main__":
-    def experiment_rt():
-        route_id = 1071
-        direction = 1
-        d_date = "2020-04-29"
-        d_time = 16800
+    """Console script for HSL GTFS data preprocessing"""
 
-        pointlist = fetch_route(route_id, direction, d_date, d_time)
-        print(pointlist)
-    
-    def experiment_gtfs():
-        print('Fetching data')
-        shapes_df, trips_df, calendar_df, routes_df = fetch_HSL_gtfs_data()
-        print('Contents of the shapes.txt:')
-        print(shapes_df.head())
-        routes = process_gtfs_shapes(shapes_df)
-        print('Processed route data:')
-        print(routes.head())
-        schedule = process_gtfs_schedule(trips_df, calendar_df, routes_df)
-        print('Processed schedule data:')
-        print(schedule.head())
-    
-    random_df = initialize_random_routes(60)
-    experiment_gtfs()
+    import sys
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', help="directory for data files output", type=str)
+    parser.add_argument('--sample_nr', help="number of routes to pick from dataset (random sample) ", type=int, default=0)
+    args = parser.parse_args()
+
+
+    if args.data_dir:
+        logger.info('Preprocessing GTFS to schdule.csv and routes files')
+        logger.info(f'using output directory {args.data_dir}')
+        preprocess_initdata_to_files(sample_nr = args.sample_nr, data_path = args.data_dir)
+
+    # previous manual testing/experimenting script
+    # theses will be executed if no arguments are given
+    else:
+
+        def experiment_rt():
+            route_id = 1071
+            direction = 1
+            d_date = "2020-04-29"
+            d_time = 16800
+
+            pointlist = fetch_route(route_id, direction, d_date, d_time)
+            print(pointlist)
+
+        def experiment_gtfs():
+            print('Fetching data')
+            shapes_df, trips_df, calendar_df, routes_df = fetch_HSL_gtfs_data()
+            print('Contents of the shapes.txt:')
+            print(shapes_df.head())
+            routes = process_gtfs_shapes(shapes_df)
+            print('Processed route data:')
+            print(routes.head())
+            schedule = process_gtfs_schedule(trips_df, calendar_df, routes_df)
+            print('Processed schedule data:')
+            print(schedule.head())
+
+        random_df = initialize_random_routes(60)
+        experiment_gtfs()
